af53eec61b6419720f2373f8c19be55c646a90ea
Kishan Aghera <55836816+kishan-aghera@users.noreply.github.com>
2021-03-20 11:03:02 +0530

    Added seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+busports = Busport.create([
+  { code: 'AHM' },
+  { code: 'RJT' }
+])
+
+buses = Bus.create([
+  { from_busport_id: busports.first.id, to_busport_id: busports.last.id, duration: 240, scheduled_on: "2020-12-25 07:00:00"},
+  { from_busport_id: busports.first.id, to_busport_id: busports.last.id, duration: 240, scheduled_on: "2020-12-25 09:00:00"},
+  { from_busport_id: busports.first.id, to_busport_id: busports.last.id, duration: 240, scheduled_on: "2020-12-25 11:00:00"},
+  { from_busport_id: busports.last.id, to_busport_id: busports.first.id, duration: 240, scheduled_on: "2020-12-26 07:00:00"},
+  { from_busport_id: busports.last.id, to_busport_id: busports.first.id, duration: 240, scheduled_on: "2020-12-26 09:00:00"},
+  { from_busport_id: busports.last.id, to_busport_id: busports.first.id, duration: 240, scheduled_on: "2020-12-26 11:00:00"}
+])
